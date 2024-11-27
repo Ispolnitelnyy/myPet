432f46afb492b90313331f3bc5302418ccdc84c8
unknown <nikolaev3005@gmaol.com>
2024-11-27 10:00:24 +0300

    webpack config stage 3 (plugin)

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
    3.1. npm install webpack webpack-cli --save-dev (ULBI: "webpack":"^5.96.1","webpack-cli":"^5.1.4")
    3.2. создаем webpack.config.js командой npx webpack --config webpack.config.js
    3.3 при заполненных entry и output в конфиге вэбпака, можем его запустить командой 'npx webpack'
+4. задаем динамический нэйминг для бандла в конфиге (комит-3d737a95879dfbbaa3b5be1b8e2a4e16a8d58f55)
+5. создаем public, index.html
+6. подтягиваем плагины вэбпака (html-webpack-plugin)
+7. вписываем в конфиг плагины и задаем template для HtmlWebpackPlugin
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
   //настройки по умолчанию для среды development
@@ -11,4 +13,10 @@ module.exports = {
     filename: "[name].[contenthash].js", //  Name берется из entry (сейчас name=bundle)
     clean: true, //для удаления, старых сборок
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),// берет за основу index.html из public
+    }),
+    new webpack.ProgressPlugin(),
+  ],
 };
