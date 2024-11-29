ce665fc37feefd5d9f131c1b81cc38497cff8659
unknown <nikolaev3005@gmaol.com>
2024-11-30 02:12:38 +0300

    refactor routes, create routes config

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 import "./styles/index.scss";
 import { useTheme } from "./components/shared/ui/themeButton/hook";
-import { HomePage } from "./pages/homePage";
+import { NavBar } from "./components/widgets/navBar";
+import { AppRouter } from "./providers";
 
 export const App = (): JSX.Element => {
   const { theme } = useTheme();
   return (
     <BrowserRouter>
       <div className={`app ${theme}`}>
-        <HomePage />
+      <NavBar />
+      <AppRouter />
       </div>
     </BrowserRouter>
   );
