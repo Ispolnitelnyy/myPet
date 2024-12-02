7db211fe968f0fa3e599c9415c3eb8043b80c5e3
unknown <nikolaev3005@gmaol.com>
2024-12-02 18:06:55 +0300

    write tests on jest fot classNames function

--- src/app/components/shared/helpers/classNames/index.test.ts
+++ src/app/components/shared/helpers/classNames/index.test.ts
@@ -3,7 +3,43 @@
 import { classNames } from "./";
 
 describe("classNames", () => {
-   test("test", () => {
+   test("с одним первым параметром", () => {
       expect(classNames("someClass")).toBe("someClass");
    });
+
+   test("с дополнительным классом", () => {
+      const expected = "someClass class1 class2";
+      expect(classNames("someClass", {}, ["class1", "class2"])).toBe(expected);
+   });
+
+   test("с модами", () => {
+      const expected = "someClass class1 class2 hovered scrollable";
+      expect(
+         classNames("someClass", { hovered: true, scrollable: true }, [
+            "class1",
+            "class2",
+         ])
+      ).toBe(expected);
+   });
+
+   test("с модами (один false)", () => {
+      const expected = "someClass class1 class2 hovered";
+      expect(
+         classNames("someClass", { hovered: true, scrollable: false }, [
+            "class1",
+            "class2",
+         ])
+      ).toBe(expected);
+   });
+
+   test("с модами (один undefined)", () => {
+    const expected = "someClass class1 class2 hovered";
+    expect(
+       classNames("someClass", { hovered: true, scrollable: undefined }, [
+          "class1",
+          "class2",
+       ])
+    ).toBe(expected);
+ });
+
 });
