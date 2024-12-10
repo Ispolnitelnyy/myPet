ab4a28ac463a766121d03bce20f039029fcd263a
unknown <nikolaev3005@gmaol.com>
2024-12-10 03:55:59 +0300

    comments

--- src/shared/ui/input/index.tsx
+++ src/shared/ui/input/index.tsx
@@ -2,6 +2,7 @@ import { classNames } from "shared/helpers/classNames";
 import cls from "./index.module.scss";
 import { InputHTMLAttributes, memo, useEffect, useRef, useState } from "react";
 
+// HTMLInputProps чтобы избежать конфликта типов, так как input по дефолту уже принимает value и onChange
 type HTMLInputProps = Omit<
    InputHTMLAttributes<HTMLInputElement>,
    "value" | "onChange"
@@ -10,12 +11,13 @@ type HTMLInputProps = Omit<
 interface InputProps extends HTMLInputProps {
    className?: string;
    value?: string;
-   onChange?: (vaalue: string) => void;
+   onChange?: (vaalue: string) => void; // меняет value
    type?: string;
-   plaseholder?: string;
-   autofocus?: boolean;
+   plaseholder?: string; // для кастомного plaseholder
+   autofocus?: boolean; // чтобы автоматом ставилась каретка при монтировании
 }
 
+// оборачиваем компонент в memo чтобы избежать лишних перерисовок
 export const Input = memo((props: InputProps) => {
    const {
       className,
@@ -26,21 +28,25 @@ export const Input = memo((props: InputProps) => {
       autofocus,
       ...otherProps
    } = props;
-   
-   const ref = useRef<HTMLInputElement>(null);
-   const [isFocused, setIsFocused] = useState(false);
+
+   const ref = useRef<HTMLInputElement>(null); // определяем ссылку на HTMLInputElement
+   const [isFocused, setIsFocused] = useState(false); // чтобы каретка не мерцала по дефолту
    const [caretPosition, setCaretPosition] = useState(0);
 
    useEffect(() => {
       if (autofocus) {
+         // устанавливаем автофокус для отобожения кретки при монтировании
          setIsFocused(true);
+         // ставим физически каретку в инпут
+         // в поле curren находится инпут и вызываем у него focus()
          ref.current?.focus();
       }
    }, [autofocus]);
 
+   // стандартный input аргументом принимает event
    const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-      onChange?.(e.target.value);
-      setCaretPosition(e.target.value.length);
+      onChange?.(e.target.value); // тут отдаем сразу value при event
+      setCaretPosition(e.target.value.length); // при event меняем позицию каретки
    };
 
    const onBlur = () => {
@@ -50,8 +56,10 @@ export const Input = memo((props: InputProps) => {
       setIsFocused(true);
    };
 
+   // для того чтобы коретка была подвижна к месту где курсор
    const onSelect: React.ReactEventHandler<HTMLInputElement> = (e) => {
       const target = e.target as HTMLInputElement;
+      // selectionStart определяет какой текст выбран
       setCaretPosition(target.selectionStart || 0);
    };
 
