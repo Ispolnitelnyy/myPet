cdfe9887f0e374ae1caa55638ae2a13d1e025dad
unknown <nikolaev3005@gmaol.com>
2024-12-10 15:56:59 +0300

    create shared/text component

--- src/features/user/authByUsername/ui/loginForm/loginForm.stories.tsx
+++ src/features/user/authByUsername/ui/loginForm/loginForm.stories.tsx
@@ -2,12 +2,15 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { LoginForm } from ".";
 import { ThemeDecorator } from "shared/configs/storybook/decorators/themeDecorator";
 import { ThemeStateEnums } from "app/providers/themeProvider/themeContext";
+import { RouterDecorator } from "shared/configs/storybook/decorators/routerDecorator";
 const meta = {
    title: "Features/LoginForm",
    component: LoginForm,
    parameters: {},
    tags: ["LoginForm component"],
    argTypes: {},
+   decorators: [RouterDecorator],
+
 } satisfies Meta<typeof LoginForm>;
 
 export default meta;
---
+++ src/shared/ui/text/index.module.scss.d.ts
@@ -0,0 +1,5 @@
+export declare const component: string;
+export declare const error: string;
+export declare const primary: string;
+export declare const text: string;
+export declare const title: string;
